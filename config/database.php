aa08ab6ea3b1c391ac802cf85e2f6bdfe748dac6
Moisés Cedeño <moisesdavidaaron@gmail.com>
2022-07-11 01:54:13 -0500

    [update] Connecting with Mongo trought DSN.

--- config/database.php
+++ config/database.php
@@ -93,14 +93,8 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', 27017),
+            'dsn' => env('DB_DSN'),
             'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'options' => [
-                'database' => 'ZIPCODESDB' // sets the authentication database required by mongo 3
-            ]
         ],
 
     ],
