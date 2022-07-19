fb93ddde694324b670b4c23b8e67e61daf43d7b0
Moisés Cedeño <moisesdavidaaron@gmail.com>
2022-07-19 16:13:32 -0500

    Update SearchZipCodeTest.php

--- tests/Feature/Controllers/ZipCodes/SearchZipCodeTest.php
+++ tests/Feature/Controllers/ZipCodes/SearchZipCodeTest.php
@@ -1,1 +1,22 @@
 
+<?php
+
+namespace Tests\Feature\Controllers\ZipCodes;
+
+use Tests\TestCase;
+
+// Core
+use Tests\Mocks\ZipCodeMock;
+
+class GetZipCodeControllerTest extends TestCase
+{
+    /** @test */
+    public function should_get_an_existing_zip_code()
+    {
+        $zipCodeNumber = ZipCodeMock::create();
+
+        $response = $this->get("/api/zip-codes/search");
+
+        $response->assertStatus(200);
+    }
+}
